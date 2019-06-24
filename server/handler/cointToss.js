873c83e0db70bdbb44cb35a1545aeba8cf325306
Daniel Kienböck <daniel.kienboeck@admiral.at>
2019-06-24 15:05:50 +0200

    .net client + some log messages, package.json updated to see abilities with "npm run"

--- server/handler/cointToss.js
+++ server/handler/cointToss.js
@@ -1,6 +1,9 @@
 "use strict";
 
 module.exports = async call => {
+    console.log(`coin toss request (${new Date().toLocaleTimeString()})`);
+    // console.table(call);
+    
     for(let i = 0; i < call.request.repetitions; i++) {
         await sleep(1000);
         let number = Math.floor(Math.random() * 100);
@@ -9,7 +12,9 @@ module.exports = async call => {
             tossValue: number
         });
     }
+
     call.end();
+    console.log('_______________________________________________________');
 }
 
 function sleep(ms) {
--- server/handler/helloHandler.js
+++ server/handler/helloHandler.js
@@ -1,6 +1,12 @@
 "use strict";
 
 module.exports = parameters => {
+    // debug messages
+    console.log(`Hello ${parameters.greeting} ${parameters.name} (${new Date().toLocaleTimeString()})`);
+    console.table(parameters);
+    console.log('_______________________________________________________');
+    
+    // actual business logic ("the magic")
     return {
         message: `Hello ${parameters.greeting} ${parameters.name}`
     }
